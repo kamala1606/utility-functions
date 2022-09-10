d6a3fef723db86b0c7f01a36c21195f8942c6a99
Karen Lara <karenm.larag@uqvirtual.edu.co>
2022-09-09 22:44:26 -0500

    Add getMonthName.js and practice git stash'

---
+++ getMonthName.js
@@ -0,0 +1,15 @@
+function getMonthName(place) {
+  const months = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may'.
+    'june',
+    'july',
+    'august',
+    'septembre',
+    'october',
+    'november',
+    'december'
+}
